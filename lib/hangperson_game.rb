2289ef039ff15b24ac47afb270d6d8f84052698a
Daniel Radding <dradding@gmail.com>
2015-02-09 18:09:44 -0800

    All tests passed!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,12 +7,15 @@ class HangpersonGame
   def initialize(new_word)
   	@word = new_word
   	@guesses = ''
-  	@wrong_guesses = ''  
+  	@wrong_guesses = ''
+    @word_with_guesses = ''
+    set_word_with_guesses
   end
 
   attr_accessor :word
   attr_accessor :guesses
   attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
 
   def self.get_random_word
     require 'uri'
@@ -28,12 +31,39 @@ class HangpersonGame
     	if not (guesses.include? letter or wrong_guesses.include? letter)
     	  if word.include? letter
     	    guesses << letter
+          update(letter)
         else
     	    wrong_guesses << letter
     	  end
     	else
     	  false
     	end
+    end
+  end
+
+  def set_word_with_guesses
+    word.each_char do |c|
+      word_with_guesses << '-'
+    end
+  end
+
+  def update(letter)
+    counter = 0
+    word.each_char do |c|
+      if c.include? letter
+        word_with_guesses[counter] = letter
+      end
+      counter = counter + 1
+    end
+  end
+
+  def check_win_or_lose
+    if word == word_with_guesses
+      :win
+    elsif wrong_guesses.length >= 7
+      :lose
+    else
+      :play
     end
   end
 end
