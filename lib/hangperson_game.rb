b540c28495fa42b43e4af3c0d3ddcff8b8527f95
Daniel Radding <dradding@gmail.com>
2015-02-09 17:07:15 -0800

    10 tests passing

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,6 +4,15 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  def initialize(new_word)
+  	@word = new_word
+  	@guesses = ''
+  	@wrong_guesses = ''  
+  end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 
   def self.get_random_word
     require 'uri'
@@ -12,4 +21,15 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  def guess(letter)
+  	if not (guesses.include? letter or wrong_guesses.include? letter)
+  	  if word.include? letter
+  	    guesses << letter
+      else
+  	    wrong_guesses << letter
+  	  end
+  	else
+  	  false
+  	end
+  end
 end
